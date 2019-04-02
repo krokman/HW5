985665d49008bad012c767abadfff547cb2628c7
Hyspan <32776601+krokman@users.noreply.github.com>
2019-04-02 14:33:48 +0300

    Initial commit

---
+++ src/MyHashMap.java
@@ -0,0 +1,26 @@
+public class MyHashMap<K, V> implements MyMap<K, V> {
+	@Override
+	public void put(K key, V value) {
+
+	}
+
+	@Override
+	public V remove(K key) {
+		return null;
+	}
+
+	@Override
+	public void clear() {
+
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public void get(K key) {
+
+	}
+}
